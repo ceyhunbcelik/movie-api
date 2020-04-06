20886fb0d0c1651f80a94cc0490156d0d3fa672f
CeyhunHouse <41891940+CeyhunHouse@users.noreply.github.com>
2020-04-06 16:18:31 +0300

    between

--- routes/movie.js
+++ routes/movie.js
@@ -5,23 +5,78 @@ const router = express.Router();
 const cMovie = require('../models/cMovie');
 
 router.get('/', (req, res) => {
-  cMovie.find({}, (err, data) => {
-    console.log(data);
+  const promise = cMovie.find({ });
+
+  promise.then((data) => {
+     res.json(data);
+  }).catch((err) => {
+    res.json(err); 
+  });
+
+});
+
+// Top 10 
+router.get('/top10', (req, res) => {
+  const promise = cMovie.find({ }).limit(10).sort({ imdb_score: -1 });
+
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+router.get('/:movie_id', (req, res, next) => {
+  const promise = cMovie.findById(req.params.movie_id);
+
+  promise.then((movie) => {
+    if(!movie){
+      next({ message: 'The movie was not found!', code: 99 });
+    }
+    else{
+      res.json(movie);
+    }
+  }) .catch((err) => {
+    res.json(err);
+  }); 
+
+});
+
+router.put('/:movie_id', (req, res, next) => {
+   const promise = cMovie.findByIdAndUpdate(
+     req.params.movie_id, 
+     req.body,
+     {
+        new: true
+     },
+   );
+
+   promise.then((movie) => {
+     if(!movie){
+       next({ message: 'The movie was not found!', code: 99 });
+     } else{
+       res.json(movie);
+     }
+   }).catch((err) => {
+     res.json(err);
+   });
+});
+
+router.delete('/:movie_id', (req, res, next) => {
+  const promise = cMovie.findByIdAndRemove(req.params.movie_id);
+
+  promise.then((movie) => {
+    if(!movie){
+      next({ message: 'The movie was not found', code: 99 });
+    } else{
+      res.json({ status: 1 });
+    }
+  }).catch((err) => {
+    res.json(err);
   });
 });
 
 router.post('/', (req, res, next) => {
-  /*
-  const { title, imdb_score, category, country, year } = req.body;
-
-  const movie = new cMovie({
-    title: title,
-    imdb_score: imdb_score,
-    category: category,
-    country: country,
-    year: year
-  });
-  */
  
   const movie = new cMovie(req.body);
 
@@ -31,16 +86,26 @@ router.post('/', (req, res, next) => {
     res.json(data)
   }).catch((err) => {
     res.json(err )
-  })
+  });
 
-  /*
-  movie.save((err, data) => {
-    if(err)
-      res.json(err);
+});
 
-    res.json(data)
-  })
-  */
+router.get('/between/:start_year/:end_year', (req, res) => {
+
+  const { start_year, end_year } = req.params;
+
+  const promise = cMovie.find(
+    { 
+      year: { "$gte": parseInt(start_year), "$lte": parseInt(end_year) }
+    }
+  );
+
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+  
 });
 
 module.exports = router;
