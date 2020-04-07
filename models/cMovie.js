6e751240f8fc57130971da2d13fa28fec290669b
CeyhunHouse <41891940+CeyhunHouse@users.noreply.github.com>
2020-04-07 16:37:40 +0300

    movie validation

--- models/cMovie.js
+++ models/cMovie.js
@@ -5,12 +5,30 @@ const MovieSchema = new Schema({
     director_id: Schema.Types.ObjectId,
     title: {
         type: String,
-        required: true
+        required: [true, '`{PATH}` alanı zorunludur'],
+        maxlength: [20, '`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) karakterden küçük olmalıdır!'],
+        minlength: [3, '`{PATH}` alanı (`{VALUE}`), ({MINLENGTH}) karakterden büyük olmalıdır!']
     },
-    category: String,
-    country: String,
-    year: Number,
-    imdb_score: Number,
+    category: {
+        type: String,
+        maxlength: 30,
+        minlength: 3
+    },
+    country: {
+        type: String,
+        maxlength: 30,
+        minlength: 3
+    },
+    year: {
+        type: Number,
+        max: 2040,
+        min: 1900
+    },
+    imdb_score: {
+        type: Number,
+        max: 10,
+        min: 0
+    },
     createdAt: {
         type: Date,
         default: Date.now
