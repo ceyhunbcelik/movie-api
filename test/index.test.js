43981d555bd24ecc3d85dcb35de1cee59215079a
CeyhunHouse <41891940+CeyhunHouse@users.noreply.github.com>
2020-04-12 14:23:23 +0300

    test describe and it

---
+++ test/index.test.js
@@ -0,0 +1,20 @@
+const server = require('../app');
+const chaiHttp = require('chai-http');
+const chai = require('chai');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Node Server', () => {
+    
+    it('(GET /) Anasayfayı döndürür', (done) => {
+        chai.request(server)
+            .get('/')
+            .end((err, res) => {
+                res.should.have.status(200);
+                done();
+            })
+    });
+    
+});
