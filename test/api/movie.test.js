131cd59eeb79bb15b49b2e044e3d42ac1bc91688
CeyhunHouse <41891940+CeyhunHouse@users.noreply.github.com>
2020-04-13 22:44:22 +0300

    director -> movie

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -69,7 +69,7 @@ describe('/api/movies test', () => {
         })
     });
 
-    describe('/GET/:director_id movie', () => {
+    describe('/GET/:movie_id movie', () => {
         it('it should GET a movie by the given id', (done) => {
             chai.request(server)
                 .get(`/api/movies/${movieId}`)
@@ -91,7 +91,7 @@ describe('/api/movies test', () => {
         });
     });
 
-    describe('/PUT/:director_id movie', () => {
+    describe('/PUT/:movie_id movie', () => {
         it('it should UPDATE a movie given by id', (done) => {
             const movie = {
                 title: '93creative',
@@ -122,7 +122,7 @@ describe('/api/movies test', () => {
         });
     });
 
-    describe('/DELETE/:director_id movie', () => {
+    describe('/DELETE/:movie_id movie', () => {
         it('it should DELETE a movie given by id', () => {
             chai.request(server)
                 .delete(`/api/movies/${movieId}`)
